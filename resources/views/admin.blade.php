80e98c4253e5b71ec6cedffbb4dcbbea3a6f0ee4
JHugoSilva <hugosilvahs31@gmail.com>
2026-04-07 22:52:35 -0300

    Ajuste Blade

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,10 +8,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-<link rel="stylesheet" href="{{ asset('template/assets/css/admin.css') }}">
-<link rel="stylesheet" href="{{ asset('template/assets/fonts/fontawesome-free/css/all.min.css') }}">
-<link rel="stylesheet" href="{{ asset('template/assets/fonts/unicons/css/line.css') }}">
-@vite('resources/js/app.js')
+        <link rel="stylesheet" href="{{ asset('template/assets/css/admin.css') }}">
+        <link rel="stylesheet" href="{{ asset('template/assets/fonts/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('template/assets/fonts/unicons/css/line.css') }}">
+        @vite('resources/js/app.js')
 
     </head>
     <body>
